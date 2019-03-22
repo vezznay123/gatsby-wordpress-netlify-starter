c381e41e46746db001bc7bc08015f1221f639c31
vezznay123 <48800072+vezznay123@users.noreply.github.com>
2019-03-22 14:53:56 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,12 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'gatsbynetliflydemo.justinwhall.com',
+        baseUrl: '3.8.118.41',
         // baseUrl: 'wpgatsby.wtf',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: 'https',
+        protocol: 'http',
         // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: false,
         auth: {},
